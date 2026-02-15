102b06e853b8a1a73a7169af936a87a5c9ede454
kbsl252 <kaanbasol25@gmail.com>
2026-02-16 00:53:01 +0100

    Modified the file to translate certain parts from French to English

--- SequenceRecon.vhd
+++ SequenceRecon.vhd
@@ -4,50 +4,51 @@ use ieee.numeric_std.all;
 
 entity SequenceRecon2 is
 port(
-	x, y, hor : in std_logic;
+	x, y, clk : in std_logic;
 	s : out std_logic);
 end SequenceRecon2;
 
 architecture archi of SequenceRecon2 is
-type state_type is (s0, s1, s2, s3, s4, s5, s6);
-signal etat_present, etat_futur : state_type;
+type state_type is (state0, state1, state2, state3, state4, state5, state6);
+signal current_state, future_state : state_type;
 begin
 
-process(hor)
+process(clk)
 begin
-	if(rising_edge(hor)) then
-		etat_present <= etat_futur;
+	if(rising_edge(clk)) then
+		current_state <= future_state;
 	end if;
 end process;
 
-process(etat_present)
+process(current_state)
 begin
-	case etat_present is
-		when s0 => if (x = '1' and y = '0') then etat_futur <= s1;
-					  else etat_futur <= s0;
+	case current_state is
+		when state0 => if (x = '1' and y = '0') then future_state <= state1;
+					  else future_state <= state0;
 					  end if;
-		when s1 => if (x = '0' and y = '0') then etat_futur <= s2;
-					  else etat_futur <= s1;
+		when state1 => if (x = '0' and y = '0') then future_state <= state2;
+					  else future_state <= state1;
 					  end if;
-		when s2 => if (x = '0' and y = '1') then etat_futur <= s3;
-					  else etat_futur <= s2;
+		when state2 => if (x = '0' and y = '1') then future_state <= state3;
+					  else future_state <= state2;
 					  end if;
-		when s3 => if (x = '0' and y = '0') then etat_futur <= s4;
-					  else etat_futur <= s3;
+		when state3 => if (x = '0' and y = '0') then future_state <= state4;
+					  else future_state <= state3;
 					  end if;
-		when s4 => if (x = '1' and y = '0') then etat_futur <= s5;
-					  else etat_futur <= s4;
+		when state4 => if (x = '1' and y = '0') then future_state <= state5;
+					  else future_state <= state4;
 					  end if;
-		when s5 => etat_futur <= s6; 
-		when s6 => if (x = '0' or y = '1') then etat_futur <= s0;
-					  else etat_futur <= s6;
+		when state5 => future_state <= state6; 
+		when state6 => if (x = '0' or y = '1') then future_state <= state0;
+					  else future_state <= state6;
 					  end if;
-		when others => etat_futur <= s0;
+		when others => future_state <= state0;
 	end case;
 end process;
 
-s <= '0' when etat_present = s6 else '1';
+s <= '0' when current_state = state6 else '1';
 
 end archi;
 
 		
+
